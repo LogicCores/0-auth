cf7d0cfd8771b6974cb576bcdf43f0bc0a92aa70
Christoph Dorn <christoph@christophdorn.com>
2015-09-15 00:25:16 -0700

    wip

--- 0-window.api.js
+++ 0-window.api.js
@@ -1,104 +1,111 @@
 
-// TODO: Load adapters as needed on demand
-
-exports.adapters = {
-    passport: require("./for/passport/0-window.api")
-}
-
-exports.forContexts = function (contexts) {
+exports.forLib = function (LIB) {
     
     var exports = {};
 
-    var Context = exports.Context = function (defaults) {
-        var self = this;
+    // TODO: Load adapters as needed on demand
     
-        var state = {};
-
-        function resetState () {
-            // We need to keep 'state.authenticated = null' for first
-            // run so an update gets triggered below.
-            if (resetState.ranOnce) {
-                state.authenticated = false;
-            } else {
-                state.authenticated = null;
-                resetState.ranOnce = true;
-            }
-            state.serverContext = {};
-            window._.merge(state, window._.cloneDeep(defaults));
-        }
-        resetState();
-
-        self.setAuthenticated = function (authenticated, serverContext) {
-            if (state.authenticated) {
-                throw new Error("Already authenticated. Must call 'reset()' first!");
-            }
-        	try {
-        	    var ctx = {};
-        	    window._.merge(ctx, window._.cloneDeep(defaults.serverContext));
-        	    window._.merge(ctx, window._.cloneDeep(serverContext));
-                state.serverContext = ctx;
-        	    if (authenticated) {
-                    state.authenticated = true;
-                    self.emit("changed:authenticated", state.authenticated);
-        	    }
-        	} catch (err) {
-        		console.error("authenticated changed error:", err.stack);
-        	}
-        }
-
-        self.getServiceContext = function (serviceAlias) {
-            if (!state.serverContext.services) return null;
-            return state.serverContext.services[serviceAlias];
-        }
-
-        self.getServerContext = function () {
-            return state.serverContext;
-        }
-    
-        self.reset = function () {
-        	try {
-        	    var wasAuthenticated = state.authenticated;
-	            resetState();
-                if (
-                    wasAuthenticated === true ||
-                    // Trigger once when 'reset()' is called before 'setAuthenticated()' so app
-                    // can init from clean locked state
-                    wasAuthenticated === null
-                ) {
-                    self.emit("changed:authenticated", state.authenticated);
-                }
-        	} catch (err) {
-        		console.error("reset changed error:", err.stack);
-        	}
-        }
-    
-        self.login = function (service) {
-    
-            // TODO: Track authentication for multiple services.
-            if (state.authenticated) {
-                throw new Error("Cannot login. Already authenticated!");
-            }
-    
-            self.emit("login", {
-                service: service
-            });
-        }
-        
-        self.logout = function (service) {
-    
-            // TODO: Track authentication for multiple services.
-            if (!state.authenticated) {
-                throw new Error("Cannot logout. Already logged out!");
-            }
-    
-            self.emit("logout", {
-                service: service
-            });
-        }
-    
+    exports.adapters = {
+        passport: require("./for/passport/0-window.api").forLib(LIB)
     }
-    Context.prototype = Object.create(window.EventEmitter.prototype);
-    Context.prototype.contexts = contexts;
-
+    
+    exports.forContexts = function (contexts) {
+        
+        var exports = {};
+    
+        var Context = exports.Context = function (defaults) {
+            var self = this;
+        
+            var state = {};
+    
+            function resetState () {
+                // We need to keep 'state.authenticated = null' for first
+                // run so an update gets triggered below.
+                if (resetState.ranOnce) {
+                    state.authenticated = false;
+                } else {
+                    state.authenticated = null;
+                    resetState.ranOnce = true;
+                }
+                state.serverContext = {};
+                LIB._.merge(state, LIB._.cloneDeep(defaults));
+            }
+            resetState();
+    
+            self.setAuthenticated = function (authenticated, serverContext) {
+                if (state.authenticated) {
+                    throw new Error("Already authenticated. Must call 'reset()' first!");
+                }
+            	try {
+            	    var ctx = {};
+            	    LIB._.merge(ctx, LIB._.cloneDeep(defaults.serverContext));
+            	    LIB._.merge(ctx, LIB._.cloneDeep(serverContext));
+                    state.serverContext = ctx;
+            	    if (authenticated) {
+                        state.authenticated = true;
+                        self.emit("changed:authenticated", state.authenticated);
+            	    }
+            	} catch (err) {
+            		console.error("authenticated changed error:", err.stack);
+            	}
+            }
+    
+            self.getServiceContext = function (serviceAlias) {
+                if (!state.serverContext.services) return null;
+                return state.serverContext.services[serviceAlias];
+            }
+    
+            self.getServerContext = function () {
+                return state.serverContext;
+            }
+        
+            self.reset = function () {
+            	try {
+            	    var wasAuthenticated = state.authenticated;
+    	            resetState();
+                    if (
+                        wasAuthenticated === true ||
+                        // Trigger once when 'reset()' is called before 'setAuthenticated()' so app
+                        // can init from clean locked state
+                        wasAuthenticated === null
+                    ) {
+                        self.emit("changed:authenticated", state.authenticated);
+                    }
+            	} catch (err) {
+            		console.error("reset changed error:", err.stack);
+            	}
+            }
+        
+            self.login = function (service) {
+        
+                // TODO: Track authentication for multiple services.
+                if (state.authenticated) {
+                    throw new Error("Cannot login. Already authenticated!");
+                }
+        
+                self.emit("login", {
+                    service: service
+                });
+            }
+            
+            self.logout = function (service) {
+        
+                // TODO: Track authentication for multiple services.
+                if (!state.authenticated) {
+                    throw new Error("Cannot logout. Already logged out!");
+                }
+        
+                self.emit("logout", {
+                    service: service
+                });
+            }
+        
+        }
+        Context.prototype = Object.create(LIB.EventEmitter.prototype);
+        Context.prototype.contexts = contexts;
+    
+        return exports;
+    }
+    
     return exports;
 }
--- for/passport/0-window.api.js
+++ for/passport/0-window.api.js
@@ -1,95 +1,102 @@
 
-exports.spin = function (context) {
+exports.forLib = function (LIB) {
+    
+    var exports = {};
 
-    var Auth = function () {
-        var self = this;
-
-        // TODO: Support multiple services
-        const SERVICE = "github";
-
-
-        function initSession () {
-
-            // This will trigger a reset event to ensure everything is initialized
-            // from a clean locked state.
-            context.reset();
-
-
-            var url = context.getServiceContext(SERVICE).urls.context;
-
-			return context.contexts.adapters.fetch.window.fetch(url).then(function(response) {
-				return response.json();
-			}).then(function (serverContext) {
-
-			    // TODO: Use declared criteria to establish when someone
-			    //       is deemed authenticated instead of using just github service.
-			    var authenticated = false;
-			    if (serverContext.services) {
-			        Object.keys(serverContext.services).forEach(function (service) {
-			            if (
-			                service === SERVICE &&
-			                serverContext.services[service].key
-		                ) {
-		                    authenticated = true;
-		                }
-			        });
-			    } else
-			    if (serverContext.authenticated === true) {
-			        authenticated = true;
-			        serverContext = {};
-			    }
-
-			    return context.setAuthenticated(authenticated, serverContext);
-			}).catch(function (err) {
-			    console.error("Error fetching session info from '" + url + "':", err.stack);
-			    throw err;
-			});
+    exports.spin = function (context) {
+    
+        var Auth = function () {
+            var self = this;
+    
+            // TODO: Support multiple services
+            const SERVICE = "github";
+    
+    
+            function initSession () {
+    
+                // This will trigger a reset event to ensure everything is initialized
+                // from a clean locked state.
+                context.reset();
+    
+    
+                var url = context.getServiceContext(SERVICE).urls.context;
+    
+    			return context.contexts.adapters.fetch.window.fetch(url).then(function(response) {
+    				return response.json();
+    			}).then(function (serverContext) {
+    
+    			    // TODO: Use declared criteria to establish when someone
+    			    //       is deemed authenticated instead of using just github service.
+    			    var authenticated = false;
+    			    if (serverContext.services) {
+    			        Object.keys(serverContext.services).forEach(function (service) {
+    			            if (
+    			                service === SERVICE &&
+    			                serverContext.services[service].key
+    		                ) {
+    		                    authenticated = true;
+    		                }
+    			        });
+    			    } else
+    			    if (serverContext.authenticated === true) {
+    			        authenticated = true;
+    			        serverContext = {};
+    			    }
+    
+    			    return context.setAuthenticated(authenticated, serverContext);
+    			}).catch(function (err) {
+    			    console.error("Error fetching session info from '" + url + "':", err.stack);
+    			    throw err;
+    			});
+            }
+    
+            context.on("login", function (info) {
+    
+                var serverContext = context.getServerContext();
+    
+                if (
+                    !serverContext ||
+                    !serverContext.services ||
+                    !serverContext.services[info.service]
+                ) {
+                    return;
+                }
+    
+                if (serverContext.services[info.service].key) {
+                    throw new Error("Already logged into service '" + info.service + "'");
+                }
+    
+                context.emit("redirect", serverContext.services[info.service].urls.login);
+            });
+    
+            context.on("logout", function (info) {
+    
+                var serverContext = context.getServerContext();
+    
+                if (
+                    !serverContext ||
+                    !serverContext.services ||
+                    !serverContext.services[info.service]
+                ) {
+                    return;
+                }
+    
+                if (!serverContext.services[info.service].key) {
+                    throw new Error("Already logged out of service '" + info.service + "'");
+                }
+    
+                context.emit("redirect", serverContext.services[info.service].urls.logout);
+            });
+    
+    
+            // Wait for listeners to attach
+            setTimeout(function () {
+                initSession();
+            }, 0);
         }
-
-        context.on("login", function (info) {
-
-            var serverContext = context.getServerContext();
-
-            if (
-                !serverContext ||
-                !serverContext.services ||
-                !serverContext.services[info.service]
-            ) {
-                return;
-            }
-
-            if (serverContext.services[info.service].key) {
-                throw new Error("Already logged into service '" + info.service + "'");
-            }
-
-            context.emit("redirect", serverContext.services[info.service].urls.login);
-        });
-
-        context.on("logout", function (info) {
-
-            var serverContext = context.getServerContext();
-
-            if (
-                !serverContext ||
-                !serverContext.services ||
-                !serverContext.services[info.service]
-            ) {
-                return;
-            }
-
-            if (!serverContext.services[info.service].key) {
-                throw new Error("Already logged out of service '" + info.service + "'");
-            }
-
-            context.emit("redirect", serverContext.services[info.service].urls.logout);
-        });
-
-
-        // Wait for listeners to attach
-        setTimeout(function () {
-            initSession();
-        }, 0);
+    
+        return new Auth(context);
     }
 
-    return new Auth(context);
+    return exports;
 }
